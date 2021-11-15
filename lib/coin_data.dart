e21352319c1fed63e598d7fe95b6778c9592928a
Hamed EL-Asma <hamedelasma@gmail.com>
2021-11-15 19:58:40 +0200

    add get data function

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -32,5 +32,17 @@ const List<String> cryptoList = [
 ];
 
 class CoinData {
+  CoinData(this.url);
 
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
 }
